12df29c5a6ba70c8f24a81379beafda4bd9eb60a
Paweł Rutkowski <pawa202@wp.pl>
2026-01-25 15:50:59 +0100

    delete trash

--- backend/plantapp/mysite/settings.py
+++ backend/plantapp/mysite/settings.py
@@ -82,7 +82,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'authentication.middleware.JWTCookieMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
