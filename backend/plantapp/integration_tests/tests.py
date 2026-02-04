ccc422d452b6172debf2435de24cb78466f943f6
tsetsuko <zosia.gba@gmail.com>
2026-02-04 17:23:24 +0100

    fix
    removed redundant import

--- backend/plantapp/integration_tests/tests.py
+++ backend/plantapp/integration_tests/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework import status
-from gardens.models import Garden, GardenUser, GardenOwner
+from gardens.models import Garden, GardenUser
 from plants.models import Plant
 from gardens.services import create_garden
 
