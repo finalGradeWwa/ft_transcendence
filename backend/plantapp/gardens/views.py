4c8dcf615bd4970b7c9927021aa7f55b09d6ce27
tsetsuko <zosia.gba@gmail.com>
2026-03-05 18:47:52 +0100

    lint fix

--- backend/plantapp/gardens/views.py
+++ backend/plantapp/gardens/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.db.models import Count, Q
+from django.db.models import Count
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.response import Response
